2aa55178412fda041a960a051ccfccdf297c04c0
S Mautner <mautner@cs.uni-freiburg.de>
2020-08-20 23:52:07 +0200

    dendro works

--- natto/optimize/dendrogram_run.py
+++ natto/optimize/dendrogram_run.py
@@ -2,12 +2,16 @@ from natto.input import load
 from natto.optimize import distances  as d
 import numpy as np 
 from functools import partial
+from lmz import *
 from basics.sgexec import sgeexecuter as sge
 from scipy.cluster import hierarchy
 from scipy.spatial.distance import squareform
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 dnames = "human1 human2 human3 human4 fluidigmc1 smartseq2 celseq2 celseq".split()
-loaders =  [ partial( load.loadgruen_single, f"../data/punk/{data}",subsample=1500) for data in dnames]
+loaders =  [ partial( load.loadgruen_single, f"../data/punk/{data}",subsample=2000) for data in dnames]
 
 
 s = sge()
@@ -15,11 +19,11 @@ NC = 16
 
 for i in Range(dnames):
     for j in range(i+1, len(dnames)):
-        s.add_job( d.rundist_2loaders , [(loaders[i],loaders[j], NC) for r in range(2)])
+        s.add_job( d.rundist_2loaders , [(loaders[i],loaders[j], NC) for r in range(20)])
 rr= s.execute()
 
 
-def dendro(mat, title):
+def dendro(mat, title, fname):
     links = squareform(mat)
     Z = hierarchy.linkage(links, 'single')
     plt.figure()
@@ -34,9 +38,10 @@ for z in range(20):
     rr_index=0
     for i in Range(dnames):
         for j in range(i+1, len(dnames)):
-            mtx[i,j]= rr[rr_index][z][0]
+            mtx[i,j]= 1- rr[rr_index][z][0]
+            mtx[j,i]= 1- rr[rr_index][z][0]
             rr_index+=1
-    dendro(mtx, f"dendrogram run:{z}")
+    dendro(mtx, f"dendrogram run:{z}", f"dendro_{z}.png")
 
 
 

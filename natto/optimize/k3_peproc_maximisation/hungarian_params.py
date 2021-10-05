664e9aef6f96c0e1df11094adf100c4dd1329544
S Mautner <mautner@cs.uni-freiburg.de>
2021-10-05 13:09:55 +0000

    true labels workaround

--- natto/optimize/k3_peproc_maximisation/hungarian_params.py
+++ natto/optimize/k3_peproc_maximisation/hungarian_params.py
@@ -13,17 +13,22 @@ from natto import input
 d2,d10,pc,o = [],[],[],[]
 x = Range(20,100,5)
 
+
+# 3k6k
 loader = lambda: input.load3k6k(pathprefix= '/home/ubuntu/repos/HungarianClustering',subsample = 1000, seed = 3)
+labels = 'labels'
+# 100 DATASET
 names = input.get71names()
 loader1 = lambda seed: input.load100(names[4],subsample=1000,path='/home/ubuntu/repos/natto/natto/data',seed=seed, remove_unlabeled=True)
 loader = lambda: [loader1(4), loader1(10)]
+labels = 'true'
 
 for pca in x:
     # load data
     data  =  loader()
     # preprocess
     zomg = process.Data().fit(list(data), visual_ftsel=False,umaps = [2,10], pca = pca, make_even=True)
-    l1,l2 = [a.obs['labels'] for a in zomg.data]
+    l1,l2 = [a.obs[labels] for a in zomg.data]
     def rate(stuff):
         a,b = stuff
         (i,j),_ = util.hungarian(a,b)

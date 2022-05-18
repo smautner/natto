c3dcf8933d328046d1765c38073a8497c5758bf3
S Mautner <mautner@cs.uni-freiburg.de>
2022-05-18 22:51:25 +0000

    cellcntc40 scatter single measures

--- natto/optimize/x40/x40_cellcnt.py
+++ natto/optimize/x40/x40_cellcnt.py
@@ -15,15 +15,16 @@ from sklearn.metrics import adjusted_rand_score, f1_score
 from sklearn.cluster import SpectralClustering,KMeans, AgglomerativeClustering
 from sklearn.metrics import precision_score
 import structout as so
+import os
 from x40 import preprocess, calc_mp20, process_labels
 
 
 def plot(xnames, folder, cleanname):
 
     labels = [f"{folder}/varcell{j}.dmp" for j in xnames]
-    xdata = map(tools.loadfile, labels)
-    # xdata = Map(lambda x: np.median(x,axis=2), xdata)
-    xdata = Map(lambda x: x[:,:,0], xdata)
+    xdata = Map(tools.loadfile, labels)
+    xdata2 = Map(lambda x: np.median(x,axis=2), xdata)
+    #xdata2 = Map(lambda x: x[:,:,0], xdata)
     labels,_ = process_labels()
     labels = np.array(labels)
 
@@ -35,22 +36,34 @@ def plot(xnames, folder, cleanname):
         return  precision_score(true, pred, average='micro')
 
     for k in [1,2,3]: # neighbors
-        y = [score(x,k) for x in xdata]
+        y = [score(x,k) for x in xdata2]
         plt.plot(jug, y, label=f'{k} cells {cleanname}')
+
+        if k == 99:
+            # optionally plitting without averaging, this changes nothing :)
+            # just reveals a bug somewhere, probably harmless so we dont track it now
+            repeats = xdata[0].shape[2]
+            currentslices =[ Map(lambda x: x[:,:,i], xdata) for i in range(repeats)]
+            y = [[score(x,k) for x in xdat] for xdat in currentslices]
+            plt.scatter(jug*repeats,y, label=f'k={k} {cleanname}')
 
 
 
 if __name__ == "__main__":
 
-    jug = Range(200,3001,400)
+    jug = Range(400,5001,400)
     np.seterr(divide='ignore', invalid='ignore')
     for j in jug:
-        preprocess(3,j, f'vcell/{j}.dmp')
+        file = f'vcell/{j}.dmp'
+        if not os.path.exists(file):
+            preprocess(3,j,file)
 
     for j in jug:
         infile =  f'vcell/{j}.dmp'
-        calc_mp20(partial(d.jaccard, ngenes=400),out=f"jacc/varcell{j}.dmp", infile=infile)
-        calc_mp20(partial(d.cosine, numgenes=400),out=f"cosi/varcell{j}.dmp",infile=infile)
+        out = f"jacc/varcell{j}.dmp"
+        if not os.path.exists(out):
+            calc_mp20(partial(d.jaccard, ngenes=400),out=out, infile=infile)
+            calc_mp20(partial(d.cosine, numgenes=400),out=f"cosi/varcell{j}.dmp",infile=infile)
 
     plot(jug,"cosi","cosine")
     plot(jug,"jacc","jaccard")
@@ -59,5 +72,4 @@ if __name__ == "__main__":
     plt.xlabel('number of cells sampled')
     plt.legend()
     plt.savefig(f"numcells.png")
-
 
